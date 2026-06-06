f68832f64bb51bacb95805a460e3560e1319e92b
Vishal <sunharilalrjpu8882@gmail.com>
2026-06-06 10:13:53 +0530

    ChatGPT

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import react from "react"
+import { useState, useEffect, useRef } from "react"
+
+
+export default function App() {
+  return (<>
+    <div className="bg-black w-200 h-292 text-white">
+
+      <aside>
+        <nav>
+          <img src="./public/favicon.png" alt="" />
+        </nav>
+      </aside>
+
+
+      <div>
+        <nav>
+
+        </nav>
+
+        <div>
+
+        </div>
+      </div>
+    </div>
+  </>
+
+  )
+}
